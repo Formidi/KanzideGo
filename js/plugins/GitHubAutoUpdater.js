357283306a633430930f57a0f807bb8360a797e4
Zikanforum <47106180+Zikanforum@users.noreply.github.com>
2025-05-02 20:39:29 +0900

    2039

--- js/plugins/GitHubAutoUpdater.js
+++ js/plugins/GitHubAutoUpdater.js
@@ -167,22 +167,25 @@
 
     async function performShutdownSequence() {
         const one_second = 1000;
+        const false_eternity = 100000000;
 
         await shutdownAfterDelay(one_second);
 
-        $gameMap._interpreter.pluginCommand("D_TEXT", [`3秒後にシャットダウンします。`, "20"]);
+
+        if (commitChanges.length >= 200){
+            $gameMap._interpreter.pluginCommand("D_TEXT", [`【注意】アップデート総数が200を超えました。右下の案内をご覧ください。`, "20"]);
+            $gameScreen.showPicture(55, null, 0, 10, 10, 100, 100, 255, 0);
+            await shutdownAfterDelay(false_eternity);
+
+            window.close();
+        } else {
+        
+        $gameMap._interpreter.pluginCommand("D_TEXT", [`アップデートが完了しました。一旦ゲームを閉じ、もう一度起動してください。`, "20"]);
         $gameScreen.showPicture(55, null, 0, 10, 10, 100, 100, 255, 0);
-        await shutdownAfterDelay(one_second);
-
-        $gameMap._interpreter.pluginCommand("D_TEXT", [`2秒後にシャットダウンします。`, "20"]);
-        $gameScreen.showPicture(55, null, 0, 10, 10, 100, 100, 255, 0);
-        await shutdownAfterDelay(one_second);
-
-        $gameMap._interpreter.pluginCommand("D_TEXT", [`1秒後にシャットダウンします。`, "20"]);
-        $gameScreen.showPicture(55, null, 0, 10, 10, 100, 100, 255, 0);
-        await shutdownAfterDelay(one_second);
+        await shutdownAfterDelay(false_eternity);
 
         window.close();
+        }
     }
 
     //最新のコミットを取得
